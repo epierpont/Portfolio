34e8b11e043237435941e97bdaf71beeb258e16d
Eric Pierpont <epierpont@gmail.com>
2019-04-23 08:33:54 -0600

    Added sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,14 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
              }
            }
          ]
+       },
+       {
+         test: /\.scss$/,
+         use: [
+           "style-loader", // creates style nodes from JS strings
+           "css-loader", // translates CSS into CommonJS
+           "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
        }
      ]
    },
