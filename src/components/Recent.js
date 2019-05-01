4c844404e3a45585fd656beab934eb8680bb6609
Eric Pierpont <epierpont@gmail.com>
2019-05-01 15:29:01 -0600

    Recent work static component in place

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -5,7 +5,30 @@ class Recent extends Component {
   render() {
     return (
       <section className="recent-work">
-        <h1>Recent Work</h1>
+        <div className="container">
+          <div className="row">
+
+            <div className="col text-center">
+              <h2>&#47;&#47; recent <span>projects</span></h2>
+            </div>{/* END .col */}
+
+          </div>{/* END .row */}
+
+          <div className="row no-gutters">
+
+          <div className="col-4 px-lg-3">
+            <div className="card">
+              <a href="#">{/*<img className="card-img-top" src="#" alt="Project" />*/}</a>
+              <div className="card-body">
+                {/*<img src="#" alt="Logo" />*/}
+                <p className="card-text"><strong>Development</strong></p>
+                <p className="card-text">Duis quis enim nec sapien fringilla interdum. Ut faucibus blandit mauris, rutrum varius lorem ultrices nec. Duis eu porta felis. Integer aliquam est sapien, a tristique ante ornare eget. Nam vel lacinia eros. Proin viverra ut mauris ac consequat. Proin malesuada dui tincidunt, euismod justo vitae, vestibulum nulla. Nunc tristique finibus sem eget feugiat.</p>
+              </div>
+            </div>
+          </div>
+
+          </div>{/* END .row */}
+        </div>{/* END .container */}
       </section>
     )
   }
