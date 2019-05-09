a376263f6dfa5b2b1758930e5a2c95f07d2114c8
Eric Pierpont <epierpont@gmail.com>
2019-05-09 06:56:21 -0400

    About section is now responsive

--- src/components/About.js
+++ src/components/About.js
@@ -16,9 +16,9 @@ class About extends Component {
         <div className="container">
           <div className="row no-gutters">
 
-            <div className="col-6">
+            <div className="col-md-6">
 
-              <h2>&#47;&#47; about <span>me</span></h2>
+              <h2 className="text-center text-md-left">&#47;&#47; about <span>me</span></h2>
 
               <p><strong>Hi there</strong>, my name is Eric Pierpont and I am a web developer based out of Steamboat Springs, CO. I grew up in Maine and attended the University of Maine where I earned my B.A. in New Media. I have been working in the industry since 2006 and have experience working for both private companies and as a contractor. I have a true passion for development which pushes me to learn something new with each and every project. It makes me smile to tell people that "I love what I do", and in the end I know that my work shows that.</p>
 
@@ -78,50 +78,50 @@ class About extends Component {
 
             <div className="col-1"></div>{/* END .col */}
 
-            <div className="col-5 experience">
+            <div className="col-md-5 experience">
 
               <div className="row no-gutters title">
-                <div className="col-3 date"></div>
-                <div className="col-9"><h2>// my <span>experience</span></h2></div>
+                <div className="col-0 col-md-3 date"></div>
+                <div className="col-12 col-md-9"><h2 className="text-center text-md-left">&#47;&#47; my <span>experience</span></h2></div>
               </div>{/* END .row */}
 
               <div className="row no-gutters">
-                <div className="col-3 date">09.14 - Today</div>
-                <div className="col-9"><strong>Swift Communications</strong> | <span>Remote</span><br/>
+                <div className="col-4 col-md-3 date">09.14 - Today</div>
+                <div className="col-8 col-md-9"><strong>Swift Communications</strong> | <span>Remote</span><br />
                   WordPress Developer</div>
               </div>{/* END .row */}
 
               <div className="row no-gutters">
-                <div className="col-3 date">06.10 - 09.14</div>
-                <div className="col-9"><strong>Independent Contractor</strong> | <span>Remote</span><br/>
+                <div className="col-4 col-md-3 date">06.10 - 09.14</div>
+                <div className="col-8 col-md-9"><strong>Independent Contractor</strong> | <span>Remote</span><br/>
                   Web Developer</div>
               </div>{/* END .row */}
 
               <div className="row no-gutters">
-                <div className="col-3 date">10.07 - 06.10</div>
-                <div className="col-9"><strong>Resort Group</strong> | <span>Steamboat Springs, CO</span><br/>
+                <div className="col-4 col-md-3 date">10.07 - 06.10</div>
+                <div className="col-8 col-md-9"><strong>Resort Group</strong> | <span>Steamboat Springs, CO</span><br/>
                   Internet Marketing Specialist</div>
               </div>{/* END .row */}
 
               <div className="row no-gutters">
-                <div className="col-3 date">11.06 - 10.07</div>
-                <div className="col-9"><strong>Sunday River</strong> | <span>Newry, ME</span><br/>
+                <div className="col-4 col-md-3 date">11.06 - 10.07</div>
+                <div className="col-8 col-md-9"><strong>Sunday River</strong> | <span>Newry, ME</span><br/>
                   Internet Marketing Specialist</div>
               </div>{/* END .row */}
 
               <hr />
 
               <div className="row no-gutters">
-                <div className="col-3 date">09.02 - 05.06</div>
-                <div className="col-9"><strong>The University of Maine </strong> | <span>Orono, ME</span><br/>
+                <div className="col-4 col-md-3 date">09.02 - 05.06</div>
+                <div className="col-8 col-md-9"><strong>The University of Maine </strong> | <span>Orono, ME</span><br/>
                   B.A. in New Media, <em>National Dean's List Society</em></div>
               </div>{/* END .row */}
 
               <hr />
 
               <div className="row no-gutters">
-                <div className="col-3 date"></div>
-                <div className="col-9"><a class="btn btn-primary" href="#" role="button">Resume</a></div>
+                <div className="col-4 col-md-3 date"></div>
+                <div className="col-8 col-md-9"><a class="btn btn-primary" href="#" role="button">Resume</a></div>
               </div>{/* END .row */}
 
             </div>{/* END .col */}
