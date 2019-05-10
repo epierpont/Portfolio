cb4f6636883a3e542cb1f87ef8f57c7155c4231d
Eric Pierpont <epierpont@gmail.com>
2019-05-10 17:58:37 -0400

    Adjusted recent project logo area and added test modal

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -14,38 +14,57 @@ class Recent extends Component {
 
           </div>{/* END .row */}
 
+          <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLongTitle">Steamboat Pilot & Today</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  ...
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="row no-gutters">
 
             <div className="col-4 steamboat">
               <div className="card">
-                <a href="#"><img className="card-img-top" src="/src/images/logo-steamboat.png" alt="Project" /></a>
+                <a href="#" className="card-img-top"><img src="/src/images/logo-steamboat.png" alt="Project" /></a>
                 <div className="card-body">
                   <p className="card-text"><strong>Development</strong></p>
                   <p className="card-text">Duis quis enim nec sapien fringilla interdum. Ut faucibus blandit mauris, rutrum varius lorem ultrices nec. Duis eu porta felis. Integer aliquam est sapien, a tristique ante ornare eget. Nam vel lacinia eros. Proin viverra ut mauris ac consequat. Proin malesuada dui tincidunt, euismod justo vitae, vestibulum nulla. Nunc tristique finibus sem eget feugiat.</p>
                 </div>
-                <a class="btn btn-primary" href="#" role="button">view</a>
+                <button className="btn btn-primary" href="#" role="button" data-toggle="modal" data-target="#exampleModalCenter">view</button>
               </div>
             </div>
 
             <div className="col-4 park-city">
               <div className="card">
-                <a href="#"><img className="card-img-top" src="/src/images/logo-park-record.png" alt="Project" /></a>
+                <a href="#" className="card-img-top"><img src="/src/images/logo-park-record.png" alt="Project" /></a>
                 <div className="card-body">
                   <p className="card-text"><strong>Development</strong></p>
                   <p className="card-text">Duis quis enim nec sapien fringilla interdum. Ut faucibus blandit mauris, rutrum varius lorem ultrices nec. Duis eu porta felis. Integer aliquam est sapien, a tristique ante ornare eget. Nam vel lacinia eros. Proin viverra ut mauris ac consequat. Proin malesuada dui tincidunt, euismod justo vitae, vestibulum nulla. Nunc tristique finibus sem eget feugiat.</p>
                 </div>
-                <a class="btn btn-primary" href="#" role="button">view</a>
+                <button className="btn btn-primary" href="#" role="button">view</button>
               </div>
             </div>
 
             <div className="col-4 health-beginnings">
               <div className="card">
-                <a href="#"><img className="card-img-top" src="/src/images/logo-healthy-beginnings.png" alt="Project" /></a>
+                <a href="#" className="card-img-top"><img src="/src/images/logo-healthy-beginnings.png" alt="Project" /></a>
                 <div className="card-body">
                   <p className="card-text"><strong>Development</strong></p>
                   <p className="card-text">Duis quis enim nec sapien fringilla interdum. Ut faucibus blandit mauris, rutrum varius lorem ultrices nec. Duis eu porta felis. Integer aliquam est sapien, a tristique ante ornare eget. Nam vel lacinia eros. Proin viverra ut mauris ac consequat. Proin malesuada dui tincidunt, euismod justo vitae, vestibulum nulla. Nunc tristique finibus sem eget feugiat.</p>
                 </div>
-                <a class="btn btn-primary" href="#" role="button">view</a>
+                <button className="btn btn-primary" href="#" role="button">view</button>
               </div>
             </div>
 
